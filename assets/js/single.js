50ec734601d64a1589c4263d2ae5534a3bff174c
James Canlas <jamescanlasjr@yahoo.com>
2021-01-21 17:38:33 -0800

    link the two html pages and made it more dynamtic for the single repo page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,24 +1,37 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
-var getRepoIssues = function(repo) {
-    console.log(repo)
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+}
+
+var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl).then(function(response) {
-        //resques was successful
-        if (response.ok) {
-            response.json().then(function(data) {
-                displayIssues(data);
+    fetch(apiUrl)
+        .then(function (response) {
+            //resques was successful
+            if (response.ok) {
+                response.json()
+                    .then(function (data) {
+                        displayIssues(data);
 
-                //check if api has paginated issues
-                if (response.headers.get("Link")) {
-                    displayWarning(repo);
-                }
-            });
-        } else {
-            alert("There was a problem with your request!");
-        }
-    });
+                        //check if api has paginated issues
+                        if (response.headers.get("Link")) {
+                            displayWarning(repo);
+                        }
+                    });
+            } else {
+                document.location.replace("./index.html");
+            }
+        });
 }
 
 var displayIssues = function (issues) {
@@ -35,7 +48,7 @@ var displayIssues = function (issues) {
         //create span to hold issues titles
         var titleEl = document.createElement("span");
         titleEl.textContent = issues[i].title;
-        
+
         //append to container
         issueEl.appendChild(titleEl);
 
@@ -55,7 +68,7 @@ var displayIssues = function (issues) {
     }
 }
 
-var displayWarning = function(repo) {
+var displayWarning = function (repo) {
     //add text to warning container
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
     var linkEl = document.createElement("a");
@@ -67,4 +80,5 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("facebook/react");
+//getRepoIssues("facebook/react");
+getRepoName();
